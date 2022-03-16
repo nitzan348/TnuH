80c828aecbcd05843f7624c413c617567cf42ca6
Dana Epshtein <depshtein1808@gmail.com>
2022-03-16 15:20:10 +0200

    Added Points to SimpleMouth class.

--- app/src/main/java/talpiot/mb/magdadmilbat/vision/detectors/SimpleMouth.java
+++ app/src/main/java/talpiot/mb/magdadmilbat/vision/detectors/SimpleMouth.java
@@ -4,6 +4,9 @@ import com.google.mediapipe.formats.proto.LandmarkProto;
 
 import talpiot.mb.magdadmilbat.vision.Point;
 import talpiot.mb.magdadmilbat.vision.VisionMaster;
+import java.io.*;
+import java.util.*;
+
 
 /**
  * Simplest implemetation of the IMouth interface. See IMouth for documentation
@@ -11,10 +14,35 @@ import talpiot.mb.magdadmilbat.vision.VisionMaster;
 public class SimpleMouth implements IMouth {
 
     private Point cornerRight, cornerLeft, top, bot, faceTop, faceBot;
+    private Vector<Point> rightCorenerToMiddleTop = new Vector<Point>(4);
+    private Vector<Point> leftCorenerToMiddleTop = new Vector<Point>(4);
+    private Vector<Point> rightCorenerToMiddleBot = new Vector<Point>(4);
+    private Vector<Point> leftCorenerToMiddleBot = new Vector<Point>(4);
     private static final double SCALER = 1000;
 
     @Override
     public void updateMouthData(LandmarkProto.NormalizedLandmarkList face) {
+
+        leftCorenerToMiddleTop.add(new Point(face.getLandmark(185)));
+        leftCorenerToMiddleTop.add(new Point(face.getLandmark(40)));
+        leftCorenerToMiddleTop.add(new Point(face.getLandmark(39)));
+        leftCorenerToMiddleTop.add(new Point(face.getLandmark(37)));
+
+        rightCorenerToMiddleTop.add(new Point (face.getLandmark(409)));
+        rightCorenerToMiddleTop.add(new Point (face.getLandmark(270)));
+        rightCorenerToMiddleTop.add(new Point (face.getLandmark(269)));
+        rightCorenerToMiddleTop.add(new Point (face.getLandmark(267)));
+
+        leftCorenerToMiddleBot.add(new Point(face.getLandmark((146))));
+        leftCorenerToMiddleBot.add(new Point(face.getLandmark((91))));
+        leftCorenerToMiddleBot.add(new Point(face.getLandmark((181))));
+        leftCorenerToMiddleBot.add(new Point(face.getLandmark((84))));
+
+        rightCorenerToMiddleBot.add(new Point(face.getLandmark((375))));
+        rightCorenerToMiddleBot.add(new Point(face.getLandmark((321))));
+        rightCorenerToMiddleBot.add(new Point(face.getLandmark((405))));
+        rightCorenerToMiddleBot.add(new Point(face.getLandmark((314))));
+
         cornerLeft = new Point(face.getLandmark(61));
         cornerRight = new Point(face.getLandmark(291));
         top = new Point(face.getLandmark(0));
