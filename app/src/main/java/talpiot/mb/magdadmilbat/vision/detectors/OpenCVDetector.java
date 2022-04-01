89eb7ee6b03e21202fa33146e60e82c0446f611b
Alexey Shapovalov <alexei.shapovalov.42@gmail.com>
2022-04-01 16:30:59 +0300

    Added skeleton

---
+++ app/src/main/java/talpiot/mb/magdadmilbat/vision/detectors/OpenCVDetector.java
@@ -0,0 +1,13 @@
+package talpiot.mb.magdadmilbat.vision.detectors;
+
+import com.google.mediapipe.framework.TextureFrame;
+
+public class OpenCVDetector {
+
+
+    public void /* openCV frame */ mediapipeToCVFrame(TextureFrame frame) {
+
+    }
+
+
+}
