4916cbe460deff839d21de82d96f38a3ad6c48d9
Roni Elbaz <elbazroni66@gmail.com>
2022-04-07 14:31:46 +0300

    Added function - Convert faceMashResult object to opencv object

--- app/src/main/java/talpiot/mb/magdadmilbat/vision/detectors/OpenCVDetector.java
+++ app/src/main/java/talpiot/mb/magdadmilbat/vision/detectors/OpenCVDetector.java
@@ -1,11 +1,19 @@
 package talpiot.mb.magdadmilbat.vision.detectors;
-
+import android.graphics.Bitmap;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 import com.google.mediapipe.framework.TextureFrame;
+import com.google.mediapipe.solutions.facemesh.FaceMeshResult;
 
 public class OpenCVDetector {
 
 
-    public void /* openCV frame */ mediapipeToCVFrame(TextureFrame frame) {
+    public Mat faceMashToCVFrame(FaceMeshResult res) {
+        Bitmap bmp = res.inputBitmap();
+        Mat mat = new Mat();
+        Bitmap bmp32 = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp32, mat);
+        return mat;
 
     }
 
